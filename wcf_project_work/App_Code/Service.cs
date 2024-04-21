aeead8245386968ed6ae19c9a000eee2947f2f92
91859 <91859@HP>
2024-04-21 19:23:14 +0530

    Add project files.

---
+++ wcf_project_work/App_Code/Service.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+	cls1 ob = new cls1();
+	public string balancecheck(int userid,string accno)
+    {
+		string sel = "select accountbalance from accounttab where userid=" + userid + "and accountno='" + accno + "'";
+		SqlDataReader dr = ob.fn_reader(sel);
+		string bal = "";
+		while (dr.Read())
+		{
+			bal = dr["accountbalance"].ToString();
+		}
+		return bal;
+	}
+
+	public string GetData(int value)
+	{
+		return string.Format("You entered: {0}", value);
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException("composite");
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+}
---
+++ wcf_project_work/App_Code/cls1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for cls1
+/// </summary>
+public class cls1
+{
+    SqlConnection con;
+    SqlCommand cmd;
+    public cls1()
+    {
+        con = new SqlConnection(@"server=HP\SQLEXPRESS;database=aspproject;Integrated Security=True");
+
+    }
+    public SqlDataReader fn_reader(string sql)
+    {
+        if (con.State == ConnectionState.Open)
+        {
+            con.Close();
+        }
+        cmd = new SqlCommand(sql, con);
+        con.Open();
+        SqlDataReader dr = cmd.ExecuteReader();
+        return dr;
+    }
+}
